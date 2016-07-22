18bd89f5d05bd0b7fe4a72c643c8bf283f4dbc66
tdrobinson94 <tdrobinson94@gmail.com>
2016-07-22 16:12:44 -0400

    set up detail page

--- src/js/controllers/single.controller.js
+++ src/js/controllers/single.controller.js
@@ -1,7 +1,19 @@
-function SingleController(){
+function SingleController($scope, $stateParams, $http, SERVER){
+
+  $scope.singleImage = {};
+
+  init();
+
+  function init(){
+
+    $http.get(SERVER.URL + $stateParams.id).then(function(res){
+      $scope.singleImage = res.data;
+
+    });
+  };
 
 }
 
-SingleController.$inject = [];
+SingleController.$inject = ['$scope', '$stateParams', '$http', 'SERVER'];
 
 export { SingleController };
